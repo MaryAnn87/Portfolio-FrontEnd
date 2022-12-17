5d6c3ea9393bc22dfd077b7ab8cadf0e31a16b41
MaryAnn87 <meliz.87@outlook.com>
2022-12-17 15:17:46 -0300

    agregado componente banner

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { LogoComponent } from './logo/logo.component';
+import { BannerComponent } from './banner/banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LogoComponent
+    LogoComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,

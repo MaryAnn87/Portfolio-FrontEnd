5d77b5642fa98c046ddb5ee552f0f4fdf8dd8668
MaryAnn87 <meliz.87@outlook.com>
2022-12-17 15:04:18 -0300

    agregado angular- componente header

--- frontend/README.md
+++ frontend/README.md
@@ -1,6 +1,6 @@
-# PortfolioFrontEnd
+# Frontend
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 15.0.3.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 15.0.4.
 
 ## Development server
 
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,14 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
+import { HeaderComponent } from './header/header.component';
+import { LogoComponent } from './logo/logo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LogoAPComponent
+    LogoComponent
   ],
   imports: [
     BrowserModule,

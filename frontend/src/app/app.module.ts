052eb18fdf9c04dbc2f6b46da370b9960445d742
MaryAnn87 <meliz.87@outlook.com>
2022-12-25 23:36:29 -0300

    Implementacion con Front
    Carpeta model (persona.service.ts) y carpeta service(persona.service.ts)
    Update de about.component.ts
    Codificacion de about.component.html para que traiga nombre de la DB

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -12,6 +11,8 @@ import { SkillsComponent } from './skills/skills.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { FooterComponent } from './footer/footer.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
+import { HttpClient, HttpClientModule } from '@angular/common/http'
+
 
 
 @NgModule({
@@ -29,8 +30,8 @@ import { NgCircleProgressModule } from 'ng-circle-progress';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgCircleProgressModule.forRoot({ })
-
+    NgCircleProgressModule.forRoot({ }),
+    HttpClientModule   
   ],
   providers: [],
   bootstrap: [AppComponent]
